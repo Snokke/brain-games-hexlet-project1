ad99583571e1d2d7770d7ba14981c4efbbc2250e
Andrei Babintsev <snakemgl.ukraine@gmail.com>
2018-03-08 00:58:08 +0200

    add brain-even game

--- src/index.js
+++ src/index.js
@@ -6,4 +6,42 @@ const welcome = () => {
   console.log(`Hello, ${actual}`);
 };
 
-export default welcome;
+const isEven = (num) => {
+  if (num % 2 === 0) {
+    return true;
+  }
+  return false;
+};
+
+const getRandomInt = max => Math.floor(Math.random() * Math.floor(max));
+
+const brainEven = () => {
+  console.log('\nWelcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no".\n');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!\n`);
+
+  const maxRandomNum = 100;
+  const yesAnswer = 'yes';
+  const noAnswer = 'no';
+  const rightAnswersForWin = 3;
+  for (let i = 0; i < rightAnswersForWin; i += 1) {
+    const randomNum = getRandomInt(maxRandomNum);
+    console.log(`Question: ${randomNum}`);
+    const answer = readlineSync.question('Your answer: ');
+
+    if ((isEven(randomNum) && answer === yesAnswer) ||
+       (!isEven(randomNum) && answer === noAnswer)) {
+      console.log('Correct!');
+    } else {
+      const rightAnswer = isEven(randomNum) ? yesAnswer : noAnswer;
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+  }
+
+  console.log(`Congratulations, ${name}!`);
+};
+
+export { brainEven, welcome };
